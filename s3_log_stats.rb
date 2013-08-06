de4d26c195041714a9cbe864171104df0beb9e17
Karl Seguin <karl@openmymind.net>
2013-08-06 23:04:27 +0800

    Added the total size. This is a high upper limit as it only considers the full size of the file, not the actual response size. Since the logs don't include the full range (just the bytes sent), it's impossible to know if a response is a duplicate from another just by the size of the actual response. At best, such a number would give us a minimum size. That isn't too useful. The maximum size isn't that useful either, but it's slightly more useful...

--- s3_log_stats.rb
+++ s3_log_stats.rb
@@ -1,14 +1,15 @@
+require 'set'
 require 'time'
 require 'geoip'
 
 root = ARGV[0]
 
-$all = Hash.new {|h,k| h[k] = {minutes: {}, hours: {}, total: []}}
+$all = Hash.new {|h,k| h[k] = {minutes: {}, hours: {}, total: [], files: Set.new, size: 0}}
 $geo = GeoIP.new(root + 'GeoIP.dat')
 
 def process(file)
   File.readlines(file).each do |line|
-    parts = line.split(' ', 16)
+    parts = line.split(' ', 17)
     size = parts[14].to_i
     next if size == 0
     date_part = parts[2][1..-1]
@@ -16,17 +17,23 @@ def process(file)
     hour = Time.strptime(date_part, '%d/%b/%Y:%H').to_i
 
     ip = parts[4]
-    url = parts[8]
-
     key = $geo.country(ip).country_code2
 
-    $all[key][:minutes][minute] = [] unless $all[key][:minutes].include?(minute)
-    $all[key][:minutes][minute] << size
+    group = $all[key]
+    group[:minutes][minute] = [] unless group[:minutes].include?(minute)
+    group[:minutes][minute] << size
+    group[:hours][hour] = [] unless group[:hours].include?(hour)
+    group[:hours][hour] << size
+    group[:total] << size
 
-    $all[key][:hours][hour] = [] unless $all[key][:hours].include?(hour)
-    $all[key][:hours][hour] << size
-
-    $all[key][:total] << size
+    url = parts[8]
+    unless group[:files].include?(url)
+      full_size = parts[15].to_i
+      unless full_size == 0
+        group[:files] << url
+        group[:size] += full_size
+      end
+    end
   end
 end
 
@@ -36,6 +43,7 @@ end
 
 data = Hash.new {|h,k| h[k] = {minutes: [], hours: []}}
 $all.each do |key, d|
+  group = data[key]
   earliest = Time.now.to_i
   latest = 0
   d[:minutes].each do |time, sizes|
@@ -44,21 +52,21 @@ $all.each do |key, d|
     elsif time > latest
       latest = time
     end
-    data[key][:minutes] << sizes.inject { |sum, x| sum += x }
+    group[:minutes] << sizes.inject { |sum, x| sum += x }
   end
-
-  data[key][:minutes].sort!
+  group[:minutes].sort!
 
   d[:hours].each do |time, sizes|
-    data[key][:hours] << sizes.inject { |sum, x| sum += x }
+    group[:hours] << sizes.inject { |sum, x| sum += x }
   end
-  data[key][:hours].sort!
+  group[:hours].sort!
 
-  data[key][:total] = d[:total].inject { |sum, x| sum += x } / ((latest - earliest) * 131072.0)
+  group[:total] = d[:total].inject { |sum, x| sum += x } / ((latest - earliest) * 131072.0)
+  group[:size] = d[:size] / 1073741824.0 #GB
 end
 
 
 data.sort{|a, b| b[1][:total] <=> a[1][:total]}.each do |key, d|
   key = key + ' ' * (16 - key.length) if key.length < 16
-  puts "#{key} \t %10.2f \t %10.2f \t %10.2f" % [d[:minutes][-1] / (60 * 131072.0), d[:hours][-1] / (3600 * 131072.0), d[:total]]
+  puts "#{key} \t %10.2f \t %10.2f \t %10.2f \t %10.2f" % [d[:minutes][-1] / (60 * 131072.0), d[:hours][-1] / (3600 * 131072.0), d[:total], d[:size]]
 end
